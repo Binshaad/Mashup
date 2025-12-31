d8820758ba7856a83b0f64464623f87fa0359660
Binshaad <binshadmuhammedp@gmail.com>
2025-12-31 22:43:49 +0530

    Add JavaDay2HW.java for item pricing calculation example

---
+++ JavaClass/JavaDay2HW.java
@@ -0,0 +1,16 @@
+public class JavaDay2HW {
+    
+    public static void main(String[] args) {
+         String name="Soap";
+         int quantity=4;
+         float price=18.75f;
+         float total= (float)(quantity*price);
+         System.out.println("The item is "+name);
+         System.out.println("Quantity = "+quantity);
+         System.out.println("Price= "+price);
+         System.out.println("Total price =  "+total);
+
+
+
+    }
+}
