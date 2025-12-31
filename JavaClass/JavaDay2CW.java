78ddb6365639c237d3ebab04e3a84179579069a5
Binshaad <binshadmuhammedp@gmail.com>
2025-12-31 10:59:49 +0530

    Add JavaDay2CW.java for book pricing and discount calculation example

---
+++ JavaClass/JavaDay2CW.java
@@ -0,0 +1,22 @@
+public class JavaDay2CW {
+    public static void main(String[] args) {
+        int numOfBOok=3;
+        int price= 275;
+        int discount=10;
+
+        System.out.println("Number of thr books= "+numOfBOok);
+        System.out.println("Price of the book = "+price);
+
+        double total= (double)(numOfBOok*price);
+        
+        System.out.println("Total price= "+total);
+        
+       
+        double discounted=(total*10)/100;
+
+        double finalBill= total-discounted;
+
+        System.out.println("The final bill is= "+finalBill);
+
+    }
+}
