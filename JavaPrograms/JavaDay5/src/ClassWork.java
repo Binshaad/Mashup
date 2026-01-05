02f4f4d274760d6a4fce196e5037b00b22fb9c2d
Binshaad <binshadmuhammedp@gmail.com>
2026-01-05 10:58:52 +0530

    Add ClassWork.java for student performance evaluation based on marks

---
+++ JavaPrograms/JavaDay5/src/ClassWork.java
@@ -0,0 +1,23 @@
+public class ClassWork {
+
+    public static void main(String[] args) {
+        int [] mark={92,69,85,72,45};
+        for(int i=0;i<mark.length;i++){
+            if(mark[i]>=90){
+                System.out.println("You are Excellent!!!!!");
+            
+            }
+
+            else if(mark[i]>=75&&mark[i]<=89){
+                System.out.println("Good Student!!!!");
+            }
+
+            else if(mark[i]>=50&&mark[i]<=74){
+                System.out.println("Avarage!!");
+            }
+            else{
+                System.out.println("Fail......Do better");
+            }
+        }
+    }
+}
