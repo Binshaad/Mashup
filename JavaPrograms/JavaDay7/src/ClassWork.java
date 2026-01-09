3c9efe36f69c5d20dea9a89d4a484ee4af2e7a0c
Binshaad <binshadmuhammedp@gmail.com>
2026-01-09 10:12:40 +0530

    Refactor ClassWork class to improve student input handling and average calculation

--- JavaPrograms/JavaDay7/src/ClassWork.java
+++ JavaPrograms/JavaDay7/src/ClassWork.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
 
 public class ClassWork {
+    static double sum = 0;
+    static double avarage = 0;
+
     public static void main(String[] args) {
-        
-    Scanner sc=new Scanner(System.in);
-    
 
-    System.out.println("Enter the number of student");
-    int noOfStdnt=sc.nextInt();
-    
-    int[] numerOf=new int[noOfStdnt];
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("Enter the Name of the student : ");
-    String name=sc.nextLine();
-    
+        System.out.println("Enter the number of student");
+        int noOfStdnt = sc.nextInt();
 
-    System.out.println("Enter Mark: ");
-    int mark=sc.nextInt();
+        int[] numerOf = new int[noOfStdnt];
 
+        String[] name = new String[noOfStdnt];
+        int[] mark = new int[noOfStdnt];
 
+        for (int i = 0; i < noOfStdnt; i++) {
 
+            System.out.println("Student  " + (i + 1));
+            name[i] = sc.nextLine();
+
+            System.out.println("Marckm of " + name[i] + " :");
+            mark[i] = sc.nextInt();
+
+        }
+
+    }
+
+    static void avrgCalcul(int[] mark) {
+
+        for (int marks:mark) {
+           sum += marks;           
+        }
+        avarage= sum/mark.length;
+        System.out.println("The avarage is:"+avarage);
+    }
 }
-}
-
